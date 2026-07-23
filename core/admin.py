f9c713981429f72a82d70a95f7094c4f73f52d3f
suyog khadka <ksuyog697@gmail.com>
2026-07-23 18:54:23 +0545

    edit frontend

--- core/admin.py
+++ core/admin.py
@@ -1,16 +1,35 @@
 from django.contrib import admin
-from .models import Project_title,Project_items,Skill,Skill_tools
-# Register your models here.
-admin.site.register(Project_title)
+from .models import Project_title, Project_items, Skill, Skill_tools, Contact, Testinomial, skill_title, skill_items
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'email', 'subject', 'phone']
+
+@admin.register(Project_title)
+class ProjectTitleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+
 @admin.register(Project_items)
 class Project_itemsAdmin(admin.ModelAdmin):
-    list_display=['id','image', 'heading','desc','language_used','url']
+    list_display = ['id', 'heading', 'category', 'language_used', 'url']
 
+@admin.register(skill_title)
+class SkillTitleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+
+@admin.register(skill_items)
+class SkillItemsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'heading', 'category', 'language_used']
 
 @admin.register(Skill)
 class SkillAdmin(admin.ModelAdmin):
-    list_display=['id','icon', 'name','desc','percent']
+    list_display = ['id', 'icon', 'name', 'desc', 'percent']
 
 @admin.register(Skill_tools)
 class Skill_toolsAdmin(admin.ModelAdmin):
-    list_display=['id','icon', 'name']
+    list_display = ['id', 'icon', 'name']
+
+@admin.register(Testinomial)
+class TestinomialAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'role', 'company', 'is_active']
+    list_filter = ['is_active']
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-8&59^$=dvls_!9l1n=ir0e+3lq+w-r@56k4^i)fa2qir!gu$!5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

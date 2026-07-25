8ed45221efe64640f475f9c2462d64e6ebdf6ab3
suyog khadka <ksuyog697@gmail.com>
2026-07-25 09:24:46 +0545

    port error in smtp

--- core/views.py
+++ core/views.py
@@ -7,7 +7,7 @@ from django.core.mail import send_mail
 from .models import *
 from datetime import date
 from django.shortcuts import render
-
+from threading import Thread
 def about(request):
     birth_date = date(2005, 4, 16) 
     today = date.today()
@@ -19,33 +19,49 @@ def about(request):
     }
     
     return render(request, 'core/about.html', context)
+
+
+
 def contact(request):
-    if request.method=="POST":
-        name=request.POST.get('name')
-        email=request.POST.get('email')
-        subject=request.POST.get('subject')
-        message=request.POST.get('message')
-        phone=request.POST.get('phone')
-        address=request.POST.get('address')
-        user=Contact(name=name,email=email,subject=subject,message=message,phone=phone,address=address)
-        user.save()
-
-        def send_email_async():
-            try:
-                send_mail(
-                    subject="Message from suyog",
-                    message="Thanks for leaving your contact we will contact you soon",
-                    from_email=None,
-                    recipient_list=[email, 'suyog697@gmail.com'],
-                    fail_silently=True,
-                )
-            except Exception:
-                pass
-
-        threading.Thread(target=send_email_async, daemon=True).start()
-        messages.success(request,f'hi {name} your form is submitted please check your email')
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        phone = request.POST.get('phone')
+        address = request.POST.get('address')
+        
+        # Save contact
+        user = Contact.objects.create(
+            name=name,
+            email=email,
+            subject=subject,
+            message=message,
+            phone=phone,
+            address=address
+        )
+        subject="Message from suyog"
+        message="Thanks for leaving your contact we will contact you soon"
+        from_email="ksuyog697@gmail.com"
+        recipient_list=[email, 'ksuyog049@gmail.com']
+        try:
+            thread = Thread(target=send_mail,kwargs={
+            "subject":subject,
+            "message": message,
+            "from_email": from_email,
+            "recipient_list": recipient_list,
+            "fail_silently": False ,
+            },daemon=True) # it is used to make work fast 
+            thread.start()
+            messages.success(request, f'Hi {name}, your form is submitted! Please check your email.')
+            
+        except Exception as e:
+            messages.warning(request, f'Hi {name}, your form is submitted. Email notification failed but we received your message.')
+        
         return redirect('contact')
-    return render(request,'core/contact.html')
+    
+    return render(request, 'core/contact.html')
+
 def home(request):
     return render(request,'core/home.html')
 def project(request):
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -17,14 +17,10 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
+SECRET_KEY= config('SECRET_KEY')
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8&59^$=dvls_!9l1n=ir0e+3lq+w-r@56k4^i)fa2qir!gu$!5'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# ✅ SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'suyogkhadak.onrender.com',
@@ -34,7 +30,6 @@ ALLOWED_HOSTS = [
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -42,11 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',  # ✅ Your app
 ]
-EXTERNAL_APPS=[
-    'core',
-]
-INSTALLED_APPS.extend(EXTERNAL_APPS)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -79,8 +71,6 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -90,8 +80,6 @@ DATABASES = {
 
 
 # Password validation
-# https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -109,39 +97,37 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/6.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
-
-STATIC_URL = 'static/'
-MEDIA_ROOT=BASE_DIR/'media'
-MEDIA_URl='/media/'
 
 
+STATIC_URL = '/static/'  
+STATIC_ROOT = BASE_DIR / 'static'  
+MEDIA_URL = '/media/'  
+MEDIA_ROOT = BASE_DIR / 'media'
 
-import os
 
-# Email Settings
+
+
+# Uncomment these and create .env file
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
-EMAIL_PORT = int(config('EMAIL_PORT', default=587))
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-DEFAULT_FROM_EMAIL = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+
 
 
 CSRF_TRUSTED_ORIGINS = [
     'https://suyogkhadak.onrender.com',
-    'http://suyogkhadak.onrender.com',  # If you're using HTTP
+    'http://suyogkhadak.onrender.com',
 ]
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
